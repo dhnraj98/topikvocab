d5bed936288b7d6717faaf406dc9e0bd7e5b0d94
dhnraj98 <dhanrajpokhrel5@gmail.com>
2025-11-23 16:15:36 +0545

    Add files via upload

---
+++ vocab1.js
@@ -0,0 +1,5 @@
+const listOne = [
+  { front: "안녕하세요", back: "नमस्ते" },
+  { front: "감사합니다", back: "धन्यवाद" },
+  { front: "사랑해요", back: "माया गर्छु" },
+];
---
+++ vocab2.js
@@ -0,0 +1,5 @@
+const listTwo = [
+  { front: "학교", back: "विद्यालय" },
+  { front: "친구", back: "साथी" },
+  { front: "좋아요", back: "राम्रो" },
+];
